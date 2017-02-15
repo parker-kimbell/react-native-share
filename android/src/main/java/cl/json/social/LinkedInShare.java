97a7ae986a97cd92ae5569de6fcfeac462dadc13
Parker.Kimbell <parker.kimbell@DS-iMAC27-16.local>
2017-02-15 13:27:15 -0800

    small touch

--- android/src/main/java/cl/json/social/LinkedInShare.java
+++ android/src/main/java/cl/json/social/LinkedInShare.java
@@ -7,13 +7,15 @@ import com.facebook.react.bridge.ReadableMap;
 
 public class LinkedInShare extends SingleShareIntent {
 
-    private static final String PACKAGE = "com.linkedin";
+    private static final String PACKAGE = "com.linkedin.android";
 
     public LinkedInShare(ReactApplicationContext reactContext) {
+        System.out.println("INSTALLED");
         super(reactContext);
     }
     @Override
     public void open(ReadableMap options) throws ActivityNotFoundException {
+        System.out.println("INSTALLED");
         super.open(options);
         //  extra params here
         this.openIntentChooser();
