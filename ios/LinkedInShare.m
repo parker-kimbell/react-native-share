19995bc12dc5d46d28511129f64985fc7dd1b38a
Parker.Kimbell <parker.kimbell@DS-iMAC27-16.local>
2017-02-15 16:13:02 -0800

    add linkedInShare ios

--- ios/LinkedInShare.m
+++ ios/LinkedInShare.m
@@ -17,7 +17,7 @@
 
     if ([options objectForKey:@"message"] && [options objectForKey:@"message"] != [NSNull null]) {
         NSString *text = [RCTConvert NSString:options[@"message"]];
-        NSString * urlLinkedIn = [NSString stringWithFormat:@"linkedin://"];
+        NSString * urlLinkedIn = [NSString stringWithFormat:@"linkedin://shareArticle?message=%@", [text stringByAppendingString: [@" " stringByAppendingString: options[@"url"]] ]];
         NSURL * linkedInURL = [NSURL URLWithString:[urlLinkedIn stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
 
         if ([[UIApplication sharedApplication] canOpenURL: linkedInURL]) {
